2f073d3d275f33f5388faea54975d658aaafb8d3
karlaromerox3 <48111726+karlaromerox3@users.noreply.github.com>
2020-08-27 20:02:21 -0500

    Initial commit

---
+++ app/src/main/java/mx/tec/example/example2_itesm_ad2020/MainActivity.java
@@ -0,0 +1,37 @@
+package mx.tec.example.example2_itesm_ad2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button myButton;
+    TextView myTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myButton = findViewById(R.id.button1);
+        myTextView = findViewById(R.id.textview1);
+
+        myTextView.setText("Hello folks!");
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //myTextView.setText("You clicked me!");
+
+                Intent myIntent = new Intent(MainActivity.this,ChildActivity.class);
+                startActivity(myIntent);
+
+
+            }
+        });
+
+    }
+}
